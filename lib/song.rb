614bbc414ca09ff5dfdd77bc979a2f52aab9e301
shiruiuiui <shirui.us@gmail.com>
2018-07-13 18:58:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,16 @@
 class Song
   
   def initialize(name, artist, genre)
-
-end
-    
-    attr_accessor :name, :artist, :genre
-  
-  lucifer = Song.new("Lucifer", "Jay-Z", "rap")
+     lucifer = Song.new("Lucifer", "Jay-Z", "rap")
    
   lucifer.name<< "Lucifer" 
    
   lucifer.artist<< "Jay-Z"
    
   lucifer.genre<< "rap"
-
 @@count = 0
 @@count += 1
+end
+    
+    attr_accessor :name, :artist, :genre
 end
