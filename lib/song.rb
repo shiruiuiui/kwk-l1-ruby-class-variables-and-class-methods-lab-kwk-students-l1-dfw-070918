2e7640129d2ecd5bdb60807e00be314d5d023484
shiruiuiui <shirui.us@gmail.com>
2018-07-13 18:59:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,11 @@ class Song
   def initialize(name, artist, genre)
      lucifer = Song.new("Lucifer", "Jay-Z", "rap")
    
-  lucifer.name<< "Lucifer" 
+    lucifer.name<< "Lucifer" 
    
-  lucifer.artist<< "Jay-Z"
+    lucifer.artist<< "Jay-Z"
    
-  lucifer.genre<< "rap"
+    lucifer.genre<< "rap"
 @@count = 0
 @@count += 1
 end
